f39b636575658989167a87c9b6e88f297594a5b9
v-chenxj59 <v-chenxj59.gd@chinatelecom.cn>
2023-11-28 22:07:40 +0800

    feat: 新增配置文件操作类，修改模板类名称

--- utils/constant.js
+++ utils/constant.js
@@ -1,9 +1,22 @@
-const HOME_DIR = process.env.HOME;
-const CACHE_FILE_NAME = ".rebar-cli";
-const DEFAULT_REGISTRY = 'https://registry.npmjs.org/';
+const HOME_DIR = process.env.HOME; // 用户根目录
+const CACHE_FILE_NAME = ".rebar-cli"; // 缓存根目录名称
+const DEFAULT_REGISTRY = 'https://registry.npmjs.org/'; // 默认源地址
+const CONFIG_FILE_NAME = ".rebarrc" // 配置文件的名称
+const DEFAULT_TEMPLATES = [ // 默认模板列表
+  {
+    name: 'vue3-admin',
+    value: '@rebar-cli/vue3-admin-template'
+  },
+  {
+    name: 'vue3-mobile',
+    value: '@rebar-cli/vue3-mobile-template'
+  }
+]
 
 module.exports = {
   HOME_DIR,
   CACHE_FILE_NAME,
-  DEFAULT_REGISTRY
+  DEFAULT_REGISTRY,
+  CONFIG_FILE_NAME,
+  DEFAULT_TEMPLATES
 };
--- utils/index.js
+++ utils/index.js
@@ -1,3 +1,5 @@
+const fs = require("fs-extra");
+
 /**
  * 睡眠函数
  * @param {number} time 睡眠时间
@@ -9,4 +11,20 @@ exports.sleep = function (time = 1000) {
       resolve();
     }, time);
   });
+};
+
+/**
+ * 解析json格式文件
+ * @param {String} path
+ * @returns
+ */
+exports.jsonFileToObject = function (path) {
+  const buffer = fs.readFileSync(path);
+  try {
+    const data = JSON.parse(buffer.toString());
+    return data;
+  } catch (err) {
+    // 如果解析异常则直接返回空对象
+    return {};
+  }
 };
