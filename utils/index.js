361ef7cdce6e0e8fe0bae064fa39fc3b7d2fe57c
v-chenxj59 <v-chenxj59.gd@chinatelecom.cn>
2023-11-29 15:12:46 +0800

    feat: config 命令核心功能

--- utils/index.js
+++ utils/index.js
@@ -24,7 +24,16 @@ exports.jsonFileToObject = function (path) {
     const data = JSON.parse(buffer.toString());
     return data;
   } catch (err) {
-    // 如果解析异常则直接返回空对象
-    return {};
+    // 如果解析异常则直接返回空
+    return null;
   }
+};
+
+/**
+ * 将json对象转换成带缩进的json字符
+ * @param {Object} data json对象
+ * @returns 格式化后的json字符串
+ */
+exports.getPrettyJson = function (data) {
+  return JSON.stringify(data, null, 2);
 };
